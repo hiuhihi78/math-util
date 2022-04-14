70c2bbea2857f17b91fc857d9895f6e141a8ea49
hiuhihi78 <leduchieu2001x@gmail.com>
2022-04-15 04:48:42 +0700

    update code to see RED ALTERT

--- test/com/hiuhihi78/mathutil/core/AdvancedFactorialTest.java
+++ test/com/hiuhihi78/mathutil/core/AdvancedFactorialTest.java
@@ -48,7 +48,7 @@ public class AdvancedFactorialTest {
                                 {3, 6},
                                 {4, 24},
                                 {5, 120},
-                                {6, 720}
+                                {6, 7200}
                               };
     }
 
